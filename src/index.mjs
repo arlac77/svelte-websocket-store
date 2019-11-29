2672b22c97097f3d8ac762e4b59709af7952222e
Markus Felten <Markus.Felten@gmx.de>
2019-11-29 01:31:29 +0100

    fix: skelteon impl

---
+++ src/index.mjs
@@ -0,0 +1,44 @@
+
+export function websocketStore(url) {
+  let socket;
+  const subscriptions = new Set();
+
+  function close() {
+    if (socket) {
+      socket.close();
+      delete socket;  
+    }
+  }
+
+  function open() {
+    if (socket) {
+      return;
+    }
+
+    socket = new WebSocket(url);
+
+    socket.onopen = event => {
+      socket.send("from browser");
+    };
+
+    socket.onerror = event => {
+      console.log(event);
+    };
+
+    socket.onmessage = event =>
+      subscriptions.forEach(subscription => subscription(event.data));
+  }
+
+  return {
+    subscribe(subscription) {
+      open();
+      subscriptions.add(subscription);
+      return () => {
+        subscriptions.delete(subscription);
+        if(substribtions.size === 0) {
+          close();
+        }
+      }
+    }
+  };
+}
