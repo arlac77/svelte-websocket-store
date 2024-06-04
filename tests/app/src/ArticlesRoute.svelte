56af1a8cc4497a8e92f1fd82ac652a9ef9bf403f
Markus Felten <Markus.Felten@gmx.de>
2024-06-04 22:31:02 +0100

    add missing tests/app/src/ArticlesRoute.svelte from template

---
+++ tests/app/src/ArticlesRoute.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Route, MasterRoute, DetailRoute } from "../../../src/index.svelte";
+  import Articles from "./Articles.svelte";
+  import Article from "./Article.svelte";
+  import ArticleLink from "./ArticleLink.svelte";
+
+  export let source;
+  export let guard;
+</script>
+
+<Route
+  path="/article"
+  factory={MasterRoute}
+  iteratorFor={source}
+  {guard}
+  component={Articles}
+>
+  <slot />
+  <Route
+    path="/:article"
+    factory={DetailRoute}
+    propertyMapping={{ article: "id" }}
+    linkComponent={ArticleLink}
+    component={Article}
+  />
+</Route>
