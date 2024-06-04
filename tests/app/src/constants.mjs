9da15f3add133eefc2827dbae396eea3d5de68ec
Markus Felten <Markus.Felten@gmx.de>
2024-06-04 22:30:22 +0100

    add missing tests/app/src/constants.mjs from template

---
+++ tests/app/src/constants.mjs
@@ -0,0 +1,5 @@
+export const base = import.meta.env.BASE_URL;
+export const name = import.meta.env.VITE_NAME;
+export const version = import.meta.env.VITE_VERSION;
+export const description = import.meta.env.VITE_DESCRIPTION;
+export const api = import.meta.env.VITE_API;
