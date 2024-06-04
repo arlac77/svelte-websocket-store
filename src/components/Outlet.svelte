9e5877476fd6915686aa2c4c3fe0807893239a13
Markus Felten <Markus.Felten@gmx.de>
2024-06-04 22:30:42 +0100

    add missing src/components/Outlet.svelte from template

---
+++ src/components/Outlet.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { getContext } from "svelte";
+  import { ROUTER } from "../util.mjs";
+  const router = getContext(ROUTER);
+</script>
+
+<svelte:component this={$router.component} {router}/>
