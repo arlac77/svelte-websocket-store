ad15bec3f1684df3987cb86f21874076dd83b3f3
Markus Felten <Markus.Felten@gmx.de>
2024-06-04 22:31:05 +0100

    add missing tests/app/src/Category.svelte from template

---
+++ tests/app/src/Category.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { ObjectLink } from "../../../src/index.svelte";
+
+  export let router;
+
+  const category = router.value;
+</script>
+
+{#if category}
+  <div class="card">
+    <h2 class="card-title routetitle">Category {category.name}</h2>
+
+    <div class="card-content">
+      {#each category.articles as article}
+        <li>
+          <ObjectLink object={article} />
+        </li>
+      {/each}
+    </div>
+  </div>
+{:else}
+  <h2 class="routetitle">No such category</h2>
+{/if}
