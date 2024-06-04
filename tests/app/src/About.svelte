64f27691ad418c6ce251e3d4368f19a2bb6ce5fb
Markus Felten <Markus.Felten@gmx.de>
2024-06-04 22:30:56 +0100

    add missing tests/app/src/About.svelte from template

---
+++ tests/app/src/About.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { name, version } from "./constants.mjs";
+</script>
+
+<div class="canvas">
+  <h2 class="routetitle">About</h2>
+  <div>
+    Example to show all features of <a
+      href="https://github.com/arlac77/svelte-guard-history-router"
+      ><b>{name}@{version}</b></a
+    >
+  </div>
+</div>
