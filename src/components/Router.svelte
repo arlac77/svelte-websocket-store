774d584e7c20bf8ab52ed6701cdb459e84a0b520
Markus Felten <Markus.Felten@gmx.de>
2024-06-04 22:30:45 +0100

    add missing src/components/Router.svelte from template

---
+++ src/components/Router.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { setContext, onMount } from "svelte";
+  import { ROUTER } from "../util.mjs";
+  import { BaseRouter } from "../base-router.mjs";
+
+  export let base;
+
+  const router = new BaseRouter([], base);
+
+  setContext(ROUTER, router);
+
+  onMount(() => router.push(router.path));
+</script>
+
+<slot />
