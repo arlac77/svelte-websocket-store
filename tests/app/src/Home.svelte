380a5caea995514950918021bb519d25aa9697b3
Markus Felten <Markus.Felten@gmx.de>
2024-06-13 12:28:14 +0200

    add missing tests/app/src/Home.svelte from template

---
+++ tests/app/src/Home.svelte
@@ -0,0 +1,16 @@
+<div class="canvas">
+  <h2 class="routetitle">Home</h2>
+  <p>Welcome to the router example code!</p>
+  <p>Things to try:</p>
+  <ul>
+    <li>
+      Navigate around with the links and buttons (notice how certain links
+      become active depending on the path)
+    </li>
+    <li>Try pressing the browsers' back and forward buttons</li>
+    <li>Manually change the URL's path</li>
+    <li>Copy paste the URL into a new brwoser window</li>
+    <li>Try refreshing the page</li>
+    <li>check router state</li>
+  </ul>
+</div>
